8f19728a2a23867a4d5b8527bc69b59e49fc7cea
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2021-11-07 20:58:23 +0100

    Preparing for multiple types of interactions

--- index.js
+++ index.js
@@ -13,21 +13,18 @@ async function main() {
   // await refreshSlashCommands(client);
 
 
-  client.on('interactionCreate', async interaction => {
-    if(!interaction.isCommand()) return;
+  client.on('interactionCreate', async interaction => {  
+    if(interaction.isCommand()) {
+      const cmd = interaction.commandName;
   
-    // console.log(interaction);
-    // console.log(interaction.options);
-  
-    const cmd = interaction.commandName;
-  
-    commands.forEach(command => {
-      if(command.name === cmd || command.aliases.indexOf(cmd) > -1) {
-        command.execute(interaction, async msg => {
-          await interaction.reply(msg);
-        });
-      }
-    });
+      commands.forEach(command => {
+        if(command.name === cmd || command.aliases.indexOf(cmd) > -1) {
+          command.execute(interaction, async msg => {
+            await interaction.reply(msg);
+          });
+        }
+      });
+    }
   });
 }
 
